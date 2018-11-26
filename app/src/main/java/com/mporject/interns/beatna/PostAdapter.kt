ae2ed955a63ba77d6e029e6c7466e065addf9d29
selimmattar <selim.mattar@esprit.tn>
2018-11-26 12:38:26 +0100

    NodeJS folder v 1.1

--- app/src/main/java/com/mporject/interns/beatna/PostAdapter.kt
+++ app/src/main/java/com/mporject/interns/beatna/PostAdapter.kt
@@ -1,26 +1,45 @@
 package com.mporject.interns.beatna
 
 import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.FragmentManager
 import android.support.v7.widget.RecyclerView.ViewHolder
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import android.support.v7.app.AppCompatActivity
+
+
 
 class PostAdapter(context: Context, resource: Int, objects: MutableList<Post>) : ArrayAdapter<Post>(context, resource, objects) {
+
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         var view :View?=null
 
         val inflater = LayoutInflater.from(context)
 
         view=inflater?.inflate(R.layout.newsfeed_item,parent,false)
-        User.getUserByUid(getItem(position).artist)
-        Song.getSongById(getItem(position).song)
-        view?.findViewById<TextView>(R.id.artistname_tv)?.text=User.user.name
-        view?.findViewById<TextView>(R.id.songname_tv)?.text=Song.song.title
+
+
+
+        view?.findViewById<TextView>(R.id.artistname_tv)?.text=getItem(position).username
+        view?.findViewById<TextView>(R.id.songname_tv)?.text=getItem(position).songname
+        view?.findViewById<ImageButton>(R.id.song_imgbtn)!!.setOnClickListener{
+        val bundle=Bundle()
+            bundle.putString("song_name",getItem(position).songname)
+
+            val SF=SongFragment()
+            SF.arguments=bundle
+            val manager = (context as AppCompatActivity).supportFragmentManager
+
+            manager.beginTransaction().replace(R.id.fragment_container,SF!!).addToBackStack(null).commit()
+        }
         return view!!
     }
+
 
 }
